74771fb530dda247249b2f2c796f2f18daa3dba9
PORTAL01\Saurabhv <vermasaurabh.monu@gmail.com>
2018-02-05 08:45:16 +0530

    Model of Application

---
+++ NextGen_Application_BookAuthor/Author.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NextGen_Application_BookAuthor
+{
+    [Table("Authors")]
+    public class Author
+
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int Aid { get; set; }
+
+        [Required]
+        public string FistName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Url { get; set; }
+
+        public ICollection<Book> book { set; get;}
+
+   
+
+    }
+}
---
+++ NextGen_Application_BookAuthor/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NextGen_Application_BookAuthor
+{
+    [Table("Books")]
+    public class Book
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int Bid { get; set; }
+     
+        [Required]
+        public string BookName { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public string Publication { get; set; }
+        [Required]
+        public string Description { get; set; }
+        
+        [ForeignKey("author")]
+         public int AuthorId { get; set;}
+        
+         public Author author { get; set; }
+      //  [DataType(DataType.Date)]
+       // [DisplayFormatAttribute(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
+       // [ScaffoldColumn(false)]
+        //public DateTime alter { get; set; }
+
+    }
+}
